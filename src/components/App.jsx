5648b26562e57e3c86be317bb754205d52f1a777
aburkowsky9 <aburkowsky9@gmail.com>
2018-10-01 21:41:18 -0700

    reverted videoListEntry to stateless

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,12 @@ import exampleVideoData from '../data/exampleVideoData.js';
 class App extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {currentVideo: false};
+    this.state = {
+      currentVideo: exampleVideoData[0]
+      // listVideos: exampleVideoData.filter((video) => {
+      //   return video !== this.state.currentVideo;
+      // })
+    }
   }
   
   onVideoListClick() {
@@ -28,7 +33,7 @@ class App extends React.Component {
         </nav>
         <div className="row">
           <div className="col-md-7">
-            <VideoPlayer video = {exampleVideoData[0]} />
+            <VideoPlayer video = {this.state.currentVideo} />
           </div>
           <div className="col-md-5">
             <VideoList videos = {exampleVideoData} />
--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,46 +1,22 @@
-import App from './App.js';
-
-class VideoListEntry extends App {
-  constructor(props) {
-    super(props);
-  }
+var VideoListEntry = (props) => (
+  <div className="video-list-entry media">
+    <div className="media-left media-middle">
+      <img className="media-object" src={props.video.snippet.thumbnails.default.url} 
+      alt="{props.video.snippet.channelTitle}" />
+    </div>
+    <div className="media-body">
+      <div className="video-list-entry-title">{props.video.snippet.title}</div>
+      <div className="video-list-entry-detail">{props.video.snippet.description}</div>
+    </div>
+  </div>
+);
   
-  render() {
-    return (
-      <div className="video-list-entry media">
-        <div className="media-left media-middle">
-          <img onClick={
-            super.onVideoListClick.bind(this),
-            super.getCurrentVideo.bind(this)
-          }
-          className="media-object" src={this.props.video.snippet.thumbnails.default.url} 
-          alt="{props.video.snippet.channelTitle}" />
-        </div>
-        <div className="media-body">
-          <div onClick={
-            super.onVideoListClick.bind(this),
-            super.getCurrentVideo.bind(this)
-          }
-          className="video-list-entry-title">{this.props.video.snippet.title}</div>
-          <div className="video-list-entry-detail">{this.props.video.snippet.description}</div>
-        </div>
-      </div>
-    )
-  }
-  
-  propTypes() { 
-    video: React.PropTypes.object.isRequired 
-  }
-  
-  
-};
-
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 
-// VideoListEntry.propTypes = {
-//   video: React.PropTypes.object.isRequired
-// };
+VideoListEntry.propTypes = {
+  video: React.PropTypes.object.isRequired
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
